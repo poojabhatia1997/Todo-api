df3b418bb6c00ca48a10f6eb938291cf4f33f161
poojabhatia1997 <poojabhatia.emilence@gmail.com>
2018-03-05 12:09:26 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-				id: 1,
-				description: 'First student',
-				completed: false
-	   	    },
-			{
-				id: 2,
-				description: 'Second user',
-				completed: false
-			}];		
+// var todos = [{
+// 				id: 1,
+// 				description: 'First student',
+// 				completed: false
+// 	   	    },
+// 			{
+// 				id: 2,
+// 				description: 'Second user',
+// 				completed: false
+// 			}];	
+var todos = [];
+var nextTodoId = 1;
+app.use(bodyParser.json());				
 
 app.get('/' , function (req, res) {
     res.send('Todo API Root');
@@ -38,6 +43,14 @@ app.get('/todos/:id', function (req,res) {
 		   else{
 				res.status(404).send();
 			}
+});
+
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    body.id = nextTodoId++;
+    todos.push(body);
+    res.json(body);
 });
 
 app.listen(PORT, function () {
